bdcd0954efe768dad0dee54a3cf97856dc9cf086
ryo <ryo.cv.0311@gmail.com>
2019-02-17 03:12:40 +0900

    dependencies are solved

--- include/Optimizer.h
+++ include/Optimizer.h
@@ -1,30 +1,47 @@
-#include <Eigen/Core>
-#include <Eigen/StdVector>
-#include <Eigen/Geometry>
-#include <iostream>
+// #include <Eigen/Core>
+// #include <Eigen/StdVector>
+// #include <Eigen/Geometry>
+// #include <iostream>
+//
+// #include <g2o/stuff/command_args.h>
+// #include "g2o/core/batch_stats.h"
+// #include "g2o/core/sparse_optimizer.h"
+// #include "g2o/core/block_solver.h"
+// #include "g2o/core/solver.h"
+// #include "g2o/core/optimization_algorithm_levenberg.h"
+// #include "g2o/core/base_vertex.h"
+// #include "g2o/core/base_binary_edge.h"
+// #include "g2o/solvers/dense/linear_solver_dense.h"
+// #include "g2o/solvers/structure_only/structure_only_solver.h"
+// #include "g2o/solvers/pcg/linear_solver_pcg.h"
+//
+// // #include "EXTERNAL/ceres/autodiff.h"
+//
+// // #if defined G2O_HAVE_CHOLMOD
+// #include "g2o/solvers/cholmod/linear_solver_cholmod.h"
+// // #elif defined G2O_HAVE_CSPARSE
+// #include "g2o/solvers/csparse/linear_solver_csparse.h"
+// // #endif
+// #include "g2o/solvers/pcg/linear_solver_pcg.h"
+// #include "g2o/core/linear_solver.h"
+// #include "g2o/core/optimization_algorithm_factory.h"
+// #include "g2o/types/slam3d/types_slam3d.h"
+// #include "g2o/types/slam3d_addons/types_slam3d_addons.h"
+// #include "g2o/stuff/macros.h"
 
-#include <g2o/stuff/command_args.h>
-#include "g2o/core/batch_stats.h"
+#include <Eigen/StdVector>
+#include <iostream>
+#include <stdint.h>
+
+#include <unordered_set>
+
 #include "g2o/core/sparse_optimizer.h"
 #include "g2o/core/block_solver.h"
 #include "g2o/core/solver.h"
+#include "g2o/core/robust_kernel_impl.h"
 #include "g2o/core/optimization_algorithm_levenberg.h"
-#include "g2o/core/base_vertex.h"
-#include "g2o/core/base_binary_edge.h"
-#include "g2o/solvers/dense/linear_solver_dense.h"
-#include "g2o/solvers/structure_only/structure_only_solver.h"
-#include "g2o/solvers/pcg/linear_solver_pcg.h"
-
-// #include "EXTERNAL/ceres/autodiff.h"
-
-// #if defined G2O_HAVE_CHOLMOD
 #include "g2o/solvers/cholmod/linear_solver_cholmod.h"
-// #elif defined G2O_HAVE_CSPARSE
-#include "g2o/solvers/csparse/linear_solver_csparse.h"
-// #endif
-#include "g2o/solvers/pcg/linear_solver_pcg.h"
-#include "g2o/core/linear_solver.h"
-#include "g2o/core/optimization_algorithm_factory.h"
-#include "g2o/types/slam3d/types_slam3d.h"
-#include "g2o/types/slam3d_addons/types_slam3d_addons.h"
-#include "g2o/stuff/macros.h"
+#include "g2o/solvers/dense/linear_solver_dense.h"
+#include "g2o/types/sba/types_six_dof_expmap.h"
+//#include "g2o/math_groups/se3quat.h"
+#include "g2o/solvers/structure_only/structure_only_solver.h"
---
+++ include/Viewer.h
@@ -0,0 +1,7 @@
+#include <Core/Core.h>
+#include <IO/IO.h>
+#include <Visualization/Visualization.h>
+
+#include <Core/Utility/Timer.h>
+
+#include <Core/Registration/ColoredICP.h>
