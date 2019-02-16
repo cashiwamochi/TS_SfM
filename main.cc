9b4489a75a29c6c0b6965c119e2d1705f62ab511
ryo <ryo.cv.0311@gmail.com>
2019-02-17 02:15:10 +0900

    now i can start to implement?

---
+++ main.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+#include <opencv2/opencv.hpp>
+
+using std::cout;
+using std::endl;
+
+void showUsage();
+
+int main(int argc, char* argv[]) {
+  if(argc != 3) {
+    showUsage();
+    return -1;
+  }
+
+
+  return 0;
+}
+
+
+void showUsage() {
+  cout << "Usage : this.out [/path/to/camera_params.yaml] "
+       << "[/path/to/config_params.yaml] "
+       << endl;
+  return;
+}
